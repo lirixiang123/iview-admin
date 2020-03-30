36665ea8c7bf7be2df20da8c6c3b3e98ac419aea
lirixiang <565539277@qq.com>
2020-03-30 18:37:36 +0800

    beattask

--- src/config/index.js
+++ src/config/index.js
@@ -18,7 +18,7 @@ export default {
    */
   baseUrl: {
     // dev: 'http://127.0.0.1:8000/api/v1/',
-    dev: 'http://192.168.189.200:8000/api/v1/',
+    dev: 'http://127.0.0.1:5000/api/v1/',
     pro: 'https://produce.com'
   },
   /**
